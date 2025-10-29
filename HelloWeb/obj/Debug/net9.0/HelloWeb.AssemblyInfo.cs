2911af5f18e5b730b7b369b7c7dc812082cd9d25
Carsten Dressler <carsten.dressler@gmail.com>
2025-10-29 16:10:26 -0400

    ..

--- HelloWeb/obj/Debug/net9.0/HelloWeb.AssemblyInfo.cs
+++ HelloWeb/obj/Debug/net9.0/HelloWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acad43800be81e0d5430b04d499928e8cfccbad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2bde3113a5990e8fa1f4dcbf854d909afac74c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
